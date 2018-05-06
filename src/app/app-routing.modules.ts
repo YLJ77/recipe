1470e4f0697e6c9c3d18aaa404ff5c4f4f08d91d
ylj <564296557@qq.com>
2018-05-06 18:27:31 +0800

    加载完当前路由资源后预加载其它路由资源

--- src/app/app-routing.modules.ts
+++ src/app/app-routing.modules.ts
@@ -1,5 +1,4 @@
-import {RouterModule, Routes} from '@angular/router';
-import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import {NgModule} from '@angular/core';
 import {HomeComponent} from './core/home/home.component';
 
@@ -10,7 +9,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules}) ],
   exports: [RouterModule]
 })
 
